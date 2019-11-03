f80d89ac1846a89b219923cb895f39d26e52cb80
Alie Boon-Dooley <asbd@uw.edu>
2019-11-03 14:50:37 -0800

    Queue is passing one test, still failing the other. I have posted on Piazza about this.

--- src/linkedlist/Queue.java
+++ src/linkedlist/Queue.java
@@ -1,22 +1,67 @@
 package linkedlist;
 
 import javax.naming.OperationNotSupportedException;
+import javax.swing.*;
 import java.util.Iterator;
 
 public class Queue<T> extends LinkedList<T> {
-    public Queue() { }
+
+    public Queue(){
+        super();
+    }
 
     @Override
     public void add(T value) {
-        /* YOUR CODE HERE */
+        this.pushBack(value);
     }
 
     @Override
     public T remove() {
-        return null;
+        return this.popFront();
     }
 
     public T peek() {
-        return null;
+        return this.peekFront();
     }
-}
+
+    @Override
+    public void add(int index, T value) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public T remove(int index) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public T peekFront() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public T peekBack() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public T popFront() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public T popBack(){
+        throw new RuntimeException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public Iterator<T> reverseIterator(){
+        throw new RuntimeException();
+    }
+
+    }
